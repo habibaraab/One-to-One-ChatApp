e297ef51723b11dd19e4c0734f3899bbf42b385a
habibaraab <habibaragab324@gmail.com>
2025-10-03 17:28:29 +0300

    Chat App

---
+++ src/main/java/com/spring/onetoonechatapp/OneToOneChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.onetoonechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneChatAppApplication.class, args);
+    }
+
+}
